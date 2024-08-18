6f4cb636656df74f6120efc0d9c3224e126ebe41
sarthak43office <patelsarthak212@gmail.com>
2024-08-18 16:37:13 +0530

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,14 +1,13 @@
 import re
 
 def check_password_strength(password):
-    # Define criteria
+a
     length_criteria = len(password) >= 8
     lowercase_criteria = re.search(r'[a-z]', password) is not None
     uppercase_criteria = re.search(r'[A-Z]', password) is not None
     number_criteria = re.search(r'[0-9]', password) is not None
     special_char_criteria = re.search(r'[\W_]', password) is not None
 
-    # Check strength
     if all([length_criteria, lowercase_criteria, uppercase_criteria, number_criteria, special_char_criteria]):
         return "Strong"
     elif length_criteria and any([lowercase_criteria, uppercase_criteria, number_criteria, special_char_criteria]):
